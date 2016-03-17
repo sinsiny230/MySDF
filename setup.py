a000eebd0e6f5480e390ddd0e95c4f2a53eee624
sinsiny230 <sinsiny230@gmail.com>
2016-03-17 13:37:12 +0900

    Added files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name    = 'MySDF',
+        version = '1.0.0',
+        py_modules = ['mysdf'],
+        author = 'Shinyoung Kim',
+        author_email = 'sinsiny230@google.com',
+        description = 'control sdf files',
+        )
